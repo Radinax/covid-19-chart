9bfd98f7c0f8d347557575a64334c9f9478edaa9
Adrian <adrberia@gmail.com>
2020-04-01 18:48:25 -0400

    Updates

---
+++ precache-manifest.0c6ab5a5a70143632f2331fc95d40a69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38d2848974262b9c050b3c5e711fabde",
+    "url": "/covid-19-chart/index.html"
+  },
+  {
+    "revision": "c3176bf142ff959b13ae",
+    "url": "/covid-19-chart/static/js/2.2ec6fbea.chunk.js"
+  },
+  {
+    "revision": "6263e813458addb410d6cbd7558bde3b",
+    "url": "/covid-19-chart/static/js/2.2ec6fbea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1508e4e1041048c2b6f8",
+    "url": "/covid-19-chart/static/js/main.f920125b.chunk.js"
+  },
+  {
+    "revision": "756a77a1a3049804a5e3",
+    "url": "/covid-19-chart/static/js/runtime-main.54a91d33.js"
+  }
+]);
